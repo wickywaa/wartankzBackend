aaa7e8a389e03525de286d8743c41ec2a95f8cd2
wickywaa <gavnewton27@gmail.com>
2022-04-28 01:13:47 +0200

    added bot list socket handler

--- app.ts
+++ app.ts
@@ -6,11 +6,11 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
+   cors: {
+    origin: ["http://localhost:3000" ,"http://192.168.0.127"],
     methods: ["GET,POST"],
     allowedHeaders: ["my-custom-header"],
-  },
+  }, 
 });
 const userRouter = require("./Routers/userRouter");
 
@@ -25,15 +25,23 @@ interface userobject {
   email: string;
   socketId: string;
 }
+
+interface botObject{
+  botName:string;
+  botId:string;
+  password:string;
+  socketId:string;
+}
+
 interface messageObject {
   userName: string;
   message: string;
 }
-let users: userobject[] =[];
+let users: userobject[] = [];
+let bots: botObject[]=[];
 let messages: messageObject[] = [];
 
 const addUser = () => {};
-
 
 io.on("connection", (socket: Socket) => {
   socket.on("disconnect", () => {
@@ -44,16 +52,25 @@ io.on("connection", (socket: Socket) => {
   });
 
   socket.on("registeruser", (user: userobject) => {
+    console.log(user)
     const filteredusers = users.filter(
       (olduser) => user.email != olduser.email
     );
-    users= filteredusers
-    console.log("users", users);
-    users =[...users,user] 
+    users = [...filteredusers, user];
+    console.log(users)
     io.sockets.emit("user_list", users);
+    io.sockets.emit("bot_list",bots)
 
     // addUser(socket)
   });
+
+  socket.on("registerBot",(bot :botObject)=>{
+    console.log('the bot is connected',bot)
+    const filteredBots = bots.filter((oldbot)=>bot.botId != oldbot.botId)
+    bots = [...filteredBots,bot]
+    console.log('here is the bots list,`',bots)
+    io.sockets.emit("bot_list",bots)
+  })
 
   socket.on("add_chat_message", (message: messageObject) => {
     messages.push(message);

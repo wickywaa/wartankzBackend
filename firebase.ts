582b25516482436b80d04a6419ebc20df4bcc024
wickywaa <gavnewton27@gmail.com>
2022-11-19 00:24:57 +0100

    removed stuff

--- firebase.ts
+++ firebase.ts
@@ -14,9 +14,6 @@ var serviceAccount = {
   "client_x509_cert_url":`${process.env.FIREBASE_ADMIN_CERT_URL}`,
 }
 
-
-require('./rawbotz-46ddb-firebase-adminsdk-rt8de-531a6cb7a8.json');
-
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(JSON.stringify(serviceAccount)),
   databaseURL: "https://rawbotz-46ddb-default-rtdb.europe-west1.firebasedatabase.app"
